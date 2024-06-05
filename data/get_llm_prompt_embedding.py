1c0b221d23484c298d21df44a5a6f75a0ea3c7c8
wangxinyufighting <wxy.782226002@qq.com>
2024-06-05 16:24:58 +0800

    update data

--- data/get_llm_prompt_embedding.py
+++ data/get_llm_prompt_embedding.py
@@ -5,11 +5,12 @@ from transformers.generation.utils import GenerationConfig
 import torch
 import json
 from tqdm import tqdm
+import os
 
 class LM_nnsight():
     def __init__(self, model_path, device="cuda", temperature=0.):
         self.device = device
-        base_model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+        base_model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, device_map='sequential', torch_dtype=torch.float16)
         tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
         if tokenizer.pad_token is None:
             tokenizer.pad_token = tokenizer.eos_token
@@ -24,8 +25,8 @@ class LM_nnsight():
             base_model.generation_config.do_sample = True
             base_model.generation_config.temperature = temperature
 
-        base_model = base_model.to(torch.float16)
-        base_model.to(self.device)
+        # base_model = base_model.to(torch.float16)
+        # base_model.to(self.device)
         base_model.eval()
         self.model = LanguageModel(base_model, tokenizer=tokenizer)
 
@@ -55,15 +56,35 @@ class LM_nnsight():
         # all_attention_states: (Layers, Tokens, Heads, 128)
 
 
-model_name = 'vicuna-13b'
+model_name = 'vicuna-7b'
+# model_name = 'Mistral-7B-Instruct-v0.2'
+# model_name = 'llama2-7b-chat-hf'
+# model_name = 'vicuna-13b'
+# model_name = 'vicuna-33b'
+
+
 model_path = f'/home/wxy/models/{model_name}'
 
-llm = LM_nnsight(model_path=model_path, device='cuda:4')
+llm = LM_nnsight(model_path=model_path, device='cuda')
 
-file_path = f'../data/datasets/gsm8k/0shot/output/0shot/train_0_1500_{model_name}_1_sample.json'
+all_hidden = []
+all_attention = []
+all_label = []
+
+data_name = 'gsm8k'
+
+file_path = f'../data/datasets/gsm8k/0shot/output/0shot/train_0_1000_{model_name}_1_sample_has_label.json'
 with open(file_path, 'r') as f:
     for line in tqdm(f.readlines()):
         data = json.loads(line.strip())
         prompt = data['prompt']
-        all_hidden_states_numpy, all_attention_states_numpy = llm.get_all_states(prompt)
+        hidden_states_numpy, attention_states_numpy = llm.get_all_states(prompt)
 
+        all_hidden.append(hidden_states_numpy[:,-1,:])
+        all_attention.append(attention_states_numpy[:,-1,:,:])
+        all_label.append(data['label'])
+
+
+np.save(f'./feature/{data_name}_{model_name}_layers.npy', all_hidden)
+np.save(f'./feature/{data_name}_{model_name}_heads.npy', all_attention)
+np.save(f'./feature/{data_name}_{model_name}_labels.npy', all_label)
