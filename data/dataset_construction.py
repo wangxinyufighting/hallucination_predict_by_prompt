0e157cf8f55372dc89131bca26cf48846588ea69
wangxinyufighting <wxy.782226002@qq.com>
2024-09-02 18:38:44 +0800

    data construction

--- data/dataset_construction.py
+++ data/dataset_construction.py
@@ -22,13 +22,13 @@ rouge = Rouge()
 def get_label(response, answers):
     if isinstance(answers, list):
         for answer in answers:
-            scores = rouge.get_scores(hypothesis=response, reference=answer)
+            scores = rouge.get_scores(response, answer)
             rougeL = scores[0]['rouge-l']['f']
             if rougeL > 0.3:
                 return 0
             
     elif isinstance(answers, str):
-        scores = rouge.get_scores(hypothesis=response, reference=answers)
+        scores = rouge.get_scores(response, answers)
         rougeL = scores[0]['rouge-l']['f']
         if rougeL > 0.3:
             return 0
@@ -36,19 +36,46 @@ def get_label(response, answers):
     return 1
 
 
+def get_trivia_qa_answers(sample):
+    answers = []
+    answers.append(sample['answer']['value'])
+    answers.append(sample['answer']['normalized_value'])
+    answers.extend(sample['answer']['normalized_aliases'])
+    answers.extend(sample['answer']['aliases'])
+
+    return list(set(answers))
+
+
 def get_trivia_qa_type(type, num_data):
+    all_data = []
+
     if type not in ["validation", "train"]:
         print(f'error! invalid type: {type}.')
         return None
 
     data = datasets.load_dataset('trivia_qa', "rc.nocontext", split=type) 
-    data_samples = data.select(random.sample(list(range(data.num_rows)), num_data)) 
 
-    path = f'{config.output_dir}/trivia_qa/{type}'
+    all_index = list(range(data.num_rows))
+    sample_index = random.sample(all_index, num_data)
+    shot_index = random.sample(list(set(all_index) - set(sample_index)), 10)
+
+    data_samples = data.select(sample_index)
+    shot_samples = data.select(shot_index)
+
+    path = f'{config.output_dir}/trivia_qa/'
     if not os.path.exists(path):
         os.makedirs(path)
 
-    data_samples.save_to_disk(path)
+    # for sample in shot_samples:
+
+    for sample in data_samples:
+        d = {}
+        d['question'] = sample['question']
+        d['answers'] = get_trivia_qa_answers(sample)
+        all_data.append(d)
+
+    with open(f'{path}/{type}_og.pkl', 'wb') as w:
+        pickle.dump(all_data, w)
 
 
 def get_trivia_qa(num_test, num_train):
@@ -89,7 +116,7 @@ def train_dataset_construction(model_name, device, data_name):
     tokenizer = AutoTokenizer.from_pretrained(path)
 
     path = f'{config.output_dir}/{data_name}/train'
-    train_dataset = datasets.load_from_disk(path).select(3)
+    train_dataset = datasets.load_from_disk(path).select(range(3))
 
     dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=1)
     with torch.no_grad():
@@ -118,10 +145,10 @@ def train_dataset_construction(model_name, device, data_name):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    with open(output_path, 'wb') as w:
+    with open(f'{output_path}/train.pkl', 'wb') as w:
         pickle.dump(results, w)
 
 if __name__ == '__main__':
-    # get_trivia_qa(300, 1500)
+    get_trivia_qa(300, 1500)
     # get_squad(300, 1500)
     train_dataset_construction('vicuna-7b', 'cuda:1', 'trivia_qa')
